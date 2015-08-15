b54f8d2389c93fc5728f23577986afa0f587dac2
hrvojebrkasic <hrcko92@gmail.com>
2015-08-15 10:48:38 +0200

    added comment

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head lang="en">
-    <meta charset="UTF-8">
+    <meta charset="UTF-8"> //change to blade
     <title>KK Sisak</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
